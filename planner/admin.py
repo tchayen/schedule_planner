255de44f1527f86a192aea497ff0813e7a2336d7
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-04-29 00:07:46 +0200

    Add descriptive view of the ChangeRequest

--- planner/admin.py
+++ planner/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Student, Subject, Teacher, Event, EventException
+from .models import Student, Subject, Teacher, Event, EventException, ChangeRequest
 
 admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Teacher)
 admin.site.register(Event)
 admin.site.register(EventException)
+admin.site.register(ChangeRequest)

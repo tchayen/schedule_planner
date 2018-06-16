deacd81b757ed38d321216923f55b320bc68c1fb
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-06-16 14:55:26 +0200

    Change behavior of colors

--- planner/models/subject.py
+++ planner/models/subject.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField(max_length=128, unique=True, db_index=True)
-    color = models.CharField(max_length=7, blank=True)
+    color = models.PositiveSmallIntegerField(blank=True)
 
     def __str__(self):
         return self.name
